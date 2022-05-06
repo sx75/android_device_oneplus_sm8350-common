18659f57d1f28c9b9cfc086256d5f3c9d3b6b4ff
John Galt <johngaltfirstrun@gmail.com>
2022-05-06 14:34:16 -0400

    sm8350-common: Enable FUSE Passthrough
    [rk134]: we have k5.4, which has FUSE Passthrough support by default.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- common.mk
+++ common.mk
@@ -191,6 +191,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
