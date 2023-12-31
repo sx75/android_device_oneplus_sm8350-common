60e80670c393a7dfc91091b56a2bf95afb8dbd30
pjgowtham <pjgowtham@gmail.com>
2023-12-31 15:38:41 +0530

    sm8350-common: Update from OOS LE2121_14.0.0.500(EX01)
    Co-authored-by: Sugakesshaa <sugakesshaa@pixelos.net>
    Change-Id: I8087a5c94317fc673dea8707eb5a185d062330f0

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.stats@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libutilscallstack.vendor:64 \
@@ -140,6 +141,7 @@ PRODUCT_PACKAGES += \
     libsdmcore \
     libsdmutils \
     libtinyxml \
+    libtinyxml2 \
     memtrack.default \
     vendor.display.config@1.0 \
     vendor.display.config@1.15.vendor \
